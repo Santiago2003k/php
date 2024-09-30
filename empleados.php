4497ab849eb19caca83d49d98dafcca781f4353b
Santiago Gontier <aguirresanti535@gmial.com>
2024-09-30 12:25:13 -0500

    trin

--- empleados.php
+++ empleados.php
@@ -42,38 +42,41 @@
         </form>
     <?php
     }  else{
-        $prof=$_REQUEST['selec'];
+        $profe=$_REQUEST['selec'];
         $h=$_REQUEST['time'];
         $salary_ing=2000000;
         $salary_tec=1000000;
         $mes=$salary_ing/26;
         $mes_t=$salary_tec/26;
         $day=$mes/8;
-        $day_tec=$mes_t/8;  
-        foreach($prof as $profe){
-            foreach($h as $time){
-                echo $time;
-                $hora_extra=[(int)$time-160];//cpnvertir a int
-                foreach($hora_extra as $he){
-                    if($he==0 && $profe=="1"){
-                        echo "<p> ing $salary_ing</p>";
+        $day_tec=$mes_t/8;
+        $pago=[];  
+        $pago_t=[];
+            foreach($h as $key => $t){
+                echo "<li>$key</li>";
+                $hora_extra[$key]=(int)$t-160;//convertir a int
+                echo "<li>$hora_extra[$key]</li>";
+                echo "<li>$profe[$key]</li>";
+                    if($hora_extra[$key]==0 && $profe[$key]=="1"){
+                        echo "<p> Su salario $salary_ing</p>";
                         echo "<br>";
-                    } elseif($he==0 && $profe="2"){
-                        echo"<p> tec $salary_tec</P";
+                    }elseif($hora_extra[$key]==0 && $profe[$key]="2"){
+                        echo"<p> tec $salary_tec</P>";
                         echo "<br>";
-                    } elseif($he>0 && $profe="1"){
-                        $pago=($day*$he)+$salary_ing;
-                        echo "pago con horas extras: i",round($pago);
+                    } elseif($hora_extra[$key]>0 && $profe[$key]="1"){
+                        $pago[$key]=($day*$hora_extra[$key])+$salary_ing;
+                        echo "pago con horas extras: ",round($pago[$key]);
                         echo "<br>";
-                    } elseif($he>0 && $profe="2"){
-                        $pago_t=($day*$he)+$salary_tec;
-                        echo "pago con horas extras: t",round($pago_t);
+                    } elseif($hora_extra[$key]>0 && $profe[$key]="2"){
+                        $pago_t=[($day_tec*$hora_extra[$key])+$salary_tec];
+                        echo $day_tec;
+                        echo "pago con horas extras: t",(round($pago_t[$key]));
                         echo "<br>";
+                    } else{
+                        echo $salary_ing;
                     }
                 }
             }
-        }
-    }
     ?>
 </div>
 </body>
